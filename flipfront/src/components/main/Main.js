5c5017a853faeb4d7e3e62d05a32f262e7a4ac8d
Steffany Sympson <ssy.marques@gmail.com>
2021-11-21 16:09:08 -0300

    Construction

---
+++ flipfront/src/components/main/Main.js
@@ -0,0 +1,13 @@
+import './Main.css';
+import Header from '../shared/header/header';
+
+const Main = () =>{
+  return (
+    <div className="background">
+      <Header />
+      <div className="area"></div>
+    </div>
+  )
+}
+
+export default Main;
---
+++ flipfront/src/components/shared/header/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import LogoTopo from '../../../assets/logo512.png';
+import './header.css';
+
+const Header = () => {
+    return(
+        <>
+            <div className='tudo-header'>
+               
+                <img className='logo' src={ LogoTopo }/>
+                <a> O que fazemos </a>
+                <a> Quem Somos </a>
+                <a> Contato </a>
+                <button className='btn-topo'>Quero Cadastrar Meu Imóvel</button>
+                
+            </div>
+        </>
+    )
+}
+export default Header;
